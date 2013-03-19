3bf25a42f282fce22b1e9cb7e3290cd009d9ca6c
Evgeny Grep <gyorms@gmail.com>
2013-03-19 17:13:09 +0200

    Added Classify generator

--- lib/generators/classify_generator.rb
+++ lib/generators/classify_generator.rb
@@ -1,17 +1,56 @@
+require 'rails/generators/active_record'
+require 'rails/generators/migration'
 require 'pp'
 class ClassifyGenerator < Rails::Generators::Base
+
+  # include Rails::Generators::ActiveRecord
+  include Rails::Generators::Migration
+  # include Rails::Generators::ResourceHelpers
 
   source_root File.expand_path('../templates', __FILE__)
 
   argument :class_name, :type => :string, :required => true
-  argument :parent_class_name, :type => :string, :required => false
   argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
 
+  class_option :parent, :type => :string, :desc => 'parent class', :default => nil
+  class_option :classify, :type => :boolean, :desc => 'is class have additional attributes?', :default => true
+
+  def self.next_migration_number(path)
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+
+  def process_attributes
+    @class_attributes = []
+    @additional_attributes = []
+    unless attributes.empty?
+      attributes.each do |attribute|
+        a = attribute.split(':')
+        if a[0] == 'attr'
+          @additional_attributes << {:name => a[1], :type => a[2] }
+        else
+          @class_attributes << {:name => a[0], :type => a[1] }
+        end
+      end
+    end
+    @additional_attributes.unshift({:name => 'class_id', :type => 'integer'}) if @additional_attributes.count{|a| a[:name] == 'class_id'} == 0
+
+  end
+
+  def process_classify_macro
+    @macro = options[:classify] ? "classify" : "classify false"
+  end
+
+  def process_migraetion_data
+
+  end
+
   def classify
-    pp class_name
-    pp parent_class_name
-    pp behavior
-    invoke "active_record:model", 'class_name'
+    pp @class_attributes
+    pp @additional_attributes
+    template "class.erb", "app/models/#{class_name.underscore}.rb"
+    migration_template "class_migration.erb", "db/migrate/create_#{class_name.underscore.gsub('/','_').pluralize}"
+    template "attr_class.erb", "app/models/#{class_name.underscore}_attribute.rb"
+    migration_template "attr_class_migration.erb", "db/migrate/create_#{"#{class_name}Attribute".underscore.gsub('/','_').pluralize}"
   end
 
 end
--- test/dummy/db/migrate/20130315135734_create_docs.rb
+++
@@ -1,8 +0,0 @@
-class CreateDocs < ActiveRecord::Migration
-  def change
-    create_table :docs do |t|
-
-      t.timestamps
-    end
-  end
-end
