0ee4cd2a269920e2f5397e28d9bc8d9fb1e40dab
Eugen <eugenci@yandex.ru>
2013-03-23 22:41:31 +0100

    Code review. Coding styles adjustments.

--- lib/active-classifier.rb
+++ lib/active-classifier.rb
@@ -3,25 +3,27 @@ module ActiveClassifier
   @class_tree = {}
 
   def self.class_tree
-    return @class_tree
+    @class_tree
   end
 
   def self.class_tree=(tree={})
-    @class_tree=tree
+    @class_tree = tree
   end
 
   def self.add_class_node(parent_node, child_node_name)
     self.get_class_node(parent_node)[child_node_name] = {}
   end
 
-  def self.get_class_node(path=[], node=nil)
+  def self.get_class_node(path = [], node = nil)
     return @class_tree if node.nil? && path.empty?
+
     node = @class_tree if node.nil? && ! path.empty?
     name = path.shift
+
     if ! name.nil? && path.empty?
-      return node[name]
+      node[name]
     else
-      return self.get_class_node(path,node[name])
+      self.get_class_node(path,node[name])
     end
   end
 
--- lib/active-classifier/classify.rb
+++ lib/active-classifier/classify.rb
@@ -6,18 +6,20 @@ module Classify
 
   module ClassMethods
 
-    def classify(clsf=true, args={})
+    def classify(clsf = true, args = {})
+
       @classified = clsf
+
       self.inheritance_queue do |cls, at|
-        if at
-          attr_accessible *self.field_names_for_class.collect{|a| a.to_sym}
-          self.has_one "attr_for_#{cls.to_s.underscore}".to_sym, :class_name => "#{cls.to_s}Attribute", :foreign_key => 'class_id', :dependent => :destroy, :autosave => true
-        end
+        next unless at
+
+        attr_accessible *self.field_names_for_class.collect{|a| a.to_sym}
+        self.has_one "attr_for_#{cls.to_s.underscore}".to_sym, :class_name => "#{cls.to_s}Attribute", :foreign_key => 'class_id', :dependent => :destroy, :autosave => true
       end
 
       parent_node = self.inheritance_array.collect{|n| n.to_s}
       child_node = parent_node.pop
-      ActiveClassifier.add_class_node(parent_node, child_node)      
+      ActiveClassifier.add_class_node(parent_node, child_node)
     end
 
     def attributes_class
@@ -26,7 +28,7 @@ module Classify
 
 
     def classified=(value)
-      @classified=value
+      @classified = value
     end
 
     def classified?
@@ -43,9 +45,7 @@ module Classify
     def inheritance_queue(reverse=true, &block)
       a = self.inheritance_array
       a = a.reverse if reverse
-      a.each do |cls|
-        yield(cls, cls.classified?)
-      end
+      a.each {|cls| yield(cls, cls.classified?) }
     end
 
     def all_class_relations
@@ -61,34 +61,39 @@ module Classify
         return cls if cls.columns_hash.key?(field_name.to_s)
         return cls.attributes_class if at && cls.attributes_class.columns_hash.key?(field_name.to_s)
       end
-      return nil
+
+      nil
     end
 
     def relation_for_field(field_name)
       self.inheritance_queue do |cls, at|
         return cls.class_relation if at && cls.attributes_class.columns_hash.key?(field_name.to_s)
       end
-      return nil
+
+      nil
     end
 
 
     def field_names_for_class
       flds = []
+
       self.inheritance_queue do |cls, at|
           flds.push(cls.column_names)
           flds.push([cls.class_relation, cls.attributes_class.column_names.reject{|n| n == 'class_id' || n == 'id'}]) if at
       end
-      return flds.flatten.uniq
+
+      flds.flatten.uniq
     end
 
     def fields_for_class
-      shared = []
-      flds = []
+      shared, flds = [], []
+
       self.inheritance_queue do |cls, at|
           shared = (shared+cls.column_names).uniq
           flds.push([cls.class_relation, cls.attributes_class.column_names.reject{|n| n == 'class_id' || n == 'id' }]) if at
       end
-      return flds.unshift(shared)
+
+      flds.unshift(shared)
     end
 
     def includes_class
@@ -99,6 +104,7 @@ module Classify
 
   def method_missing(method_name, *args, &block)
     match_data = method_name.to_s.match(/^(\w+)(|=)$/)
+
     if match_data && rel = self.class.relation_for_field(match_data[1])
       attrs = self.send(rel) || self.send("build_#{rel}")
       raise "#{rel} was not built" if attrs.nil?
@@ -106,11 +112,14 @@ module Classify
     else
       super(method_name, *args, &block)
     end
+
   end
 
   def respond_to?(name, *args)
     name.to_s =~ /^(\w+)(|=)$/
+
     return true if self.class.field_names_for_class.include?($1)
+
     super(name, *args)
   end
 
